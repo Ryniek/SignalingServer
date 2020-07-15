a44da3af6ea953fd26cde125cd03c402f29579d5
Michal Rynski <michalrynski96@gmail.com>
2020-07-15 11:27:25 +0200

    first commit

---
+++ src/main/java/pl/rynski/jitsivideomodule/JitsiVideoModuleApplication.java
@@ -0,0 +1,13 @@
+package pl.rynski.jitsivideomodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JitsiVideoModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JitsiVideoModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rynski/jitsivideomodule/JitsiVideoModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rynski.jitsivideomodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JitsiVideoModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
